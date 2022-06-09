ecd722dd3d68c8149550784d865158246a7e0a06
proger <glotovmaks97@gmail.com>
2022-06-09 16:56:58 +0300

    check

--- js/chat.js
+++ js/chat.js
@@ -1,9 +1,3 @@
 const iframeURL = 'https://jivo.chat/lsBMRYJJ5x';
 
 const iframe = document.querySelector('#chat');
-
-// iframe.addEventListener('load',() => {
-//   console.log('ok');
-// })
-
-// iframe.setAttribute('src', iframeURL);
--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+
   const regax =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
   const slidersLength = 4;
   let activeSlider = 0;
@@ -156,4 +157,46 @@ if (regax.test(navigator.userAgent)) {
       el.classList.remove('btn-active');
     })
   }
+// видео
+const iframePlayer = document.querySelector('#youtube-video');
+const customBtnPlay = document.querySelector('.video__play-btn');
+const customPreview = document.querySelector('.video__preview');
+
+customBtnPlay.addEventListener('click', () => {
+  iframePlayer.setAttribute('src', iframePlayer.dataset.src);
+  iframePlayer.classList.add('youtube-video--show');
+  customPreview.style.display = 'none';
+  customBtnPlay.style.display = 'none';
+})
+  ///main
+  const screenWidth = window.screen.width;
+
+if (screenWidth <= 520) {
+  const body = document.querySelector('body');
+  const srcipt = document.createElement('script');
+  srcipt.src = '//code-ya.jivosite.com/widget/IFj9yA1mWA';
+  srcipt.setAttribute('async', true);
+  body.appendChild(srcipt);
+}
+
+const hamburgerMenu = document.querySelector('#menu__toggle');
+
+hamburgerMenu.addEventListener('change', (e)=> {
+  e.preventDefault();
+  hamburgerMenu.setAttribute('checked', !hamburgerMenu.checked);
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    hamburgerMenu.checked = false;
+    const blockID = anchor.getAttribute('href').substr(1)
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
 });
